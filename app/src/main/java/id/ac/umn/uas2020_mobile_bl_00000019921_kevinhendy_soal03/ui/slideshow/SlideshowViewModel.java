5e4c0fd62a31184b02ff5128d2f587f6c90e4a92
kevinnhendy <kevin.hendy@student.umn.ac.id>
2020-06-02 13:47:56 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/umn/uas2020_mobile_bl_00000019921_kevinhendy_soal03/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package id.ac.umn.uas2020_mobile_bl_00000019921_kevinhendy_soal03.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/test/java/id/ac/umn/uas2020_mobile_bl_00000019921_kevinhendy_soal03/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package id.ac.umn.uas2020_mobile_bl_00000019921_kevinhendy_soal03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
